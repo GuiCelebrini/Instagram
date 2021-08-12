5a5b8d47b1b6d8def6718c5074ddb61b3ed3b3a7
GuiCelebrini <celebrinig@gmail.com>
2021-08-12 19:12:09 -0300

    Project created and added in a Firebase project

---
+++ app/src/main/java/com/android/guicelebrini/instagram/MainActivity.java
@@ -0,0 +1,21 @@
+package com.android.guicelebrini.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
