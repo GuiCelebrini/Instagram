9df3af80a4923a53ff196cfc26548322f903fce7
GuiCelebrini <celebrinig@gmail.com>
2021-08-13 23:28:41 -0300

    Created method to pass from LoginActivity to RegisterActivity

--- app/src/main/java/com/android/guicelebrini/instagram/activity/LoginActivity.java
+++ app/src/main/java/com/android/guicelebrini/instagram/activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.android.guicelebrini.instagram.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.android.guicelebrini.instagram.R;
 
@@ -12,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goToRegisterActivity(View view){
+        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(intent);
     }
 }
