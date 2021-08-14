8c57fd893f10226c22aeda7be2ae1ff797d77cf9
GuiCelebrini <celebrinig@gmail.com>
2021-08-13 23:01:10 -0300

    Login screen design created

--- app/src/main/java/com/android/guicelebrini/instagram/activity/MainActivity.java
+++ app/src/main/java/com/android/guicelebrini/instagram/activity/MainActivity.java
@@ -1,16 +1,16 @@
-package com.android.guicelebrini.instagram;
+package com.android.guicelebrini.instagram.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.android.guicelebrini.instagram.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
