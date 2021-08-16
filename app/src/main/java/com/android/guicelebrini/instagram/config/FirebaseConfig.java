c5d56a458331203af46df52290efa0827aa35ac2
GuiCelebrini <celebrinig@gmail.com>
2021-08-16 13:26:49 -0300

    Created a config file to get instances from Firebase

---
+++ app/src/main/java/com/android/guicelebrini/instagram/config/FirebaseConfig.java
@@ -0,0 +1,27 @@
+package com.android.guicelebrini.instagram.config;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class FirebaseConfig {
+
+    private static FirebaseAuth auth;
+    private static FirebaseFirestore firestore;
+
+    public static FirebaseAuth getFirebaseAuthInstance(){
+        if (auth != null) {
+            auth = FirebaseAuth.getInstance();
+        }
+
+        return auth;
+    }
+
+    public static FirebaseFirestore getFirestoreInstance(){
+        if (firestore != null) {
+            firestore = FirebaseFirestore.getInstance();
+        }
+
+        return firestore;
+    }
+
+}
